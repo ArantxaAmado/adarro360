ade296fea0762ad905abd0d2ceea11a73e05bb34
ArantxaAmado <153811977+ArantxaAmado@users.noreply.github.com>
2026-05-10 18:21:15 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -91,12 +91,12 @@ function init3DForScreen(targetId) {
 
   if (targetId === 'anfora') {
     containerId = 'd-container-piece';
-    modelPath = 'assets/models/anfora.glb';
+    modelPath = 'adarro360/assets/models/anfora.glb';
   }
 
   if (targetId === 'visor') {
     containerId = 'd-container-ra';
-    modelPath = 'assets/models/villa_darro.glb';
+    modelPath = 'adarro360/assets/models/villa_darro.glb';
   }
 
   console.log("[App] Inicialitzant visor:", containerId);
@@ -121,7 +121,7 @@ function resetCamera() { window.resetCamera3D?.(); }
 // AUDIO
 // --------------------------------------------------------------------------
 function toggleAudio() {
-  if (!currentAudio) currentAudio = new Audio('assets/audio/historia_darro.mp3');
+  if (!currentAudio) currentAudio = new Audio('/adarro360/assets/audio/historia_darro.mp3');
   const icon = document.querySelector('.play-btn span');
 
   if (currentAudio.paused) {
